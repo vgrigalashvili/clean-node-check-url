9035ab31a3595d691a357f4ed9616ce8cbf6bfbc
vgrigalashvili <lado.grigalashvili@gmail.com>
2022-06-01 17:58:53 +0400

    Adding: CLI.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 // Dependencies.
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 // Declare the app.
 const app = {};
@@ -19,6 +20,11 @@ app.init = function () {
 
     // Start the workers.
     workers.init();
+
+    // Start CLI, but make sure it starts last.
+    setTimeout(function () {
+        cli.init();
+    }, 50);
 };
 
 // Execute.
